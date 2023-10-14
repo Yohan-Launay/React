38f6540de44edbed354ea06e37529faf3dd8aa16
Yohan-Launay <yohandeveloppe@gmail.com>
2023-10-14 17:28:26 +0200

    La syntaxe JSX

--- 03_tuto_grafikart/src/App.jsx
+++ 03_tuto_grafikart/src/App.jsx
@@ -1,6 +1,34 @@
+const title = "Bonjour les <strong>gens</strong>";
+const style = {color: 'red', backgroundColor: 'blue'};
+const showTitle = false;
+const todos = [
+    'Présenter react',
+    'Présenter le JSX',
+    'Créer des composants'
+]
+
 
 function App() {
-  return 'Bonjour les gens'
+  return <>
+    <Title color="green" id="monid" className="demo" data-demo="demo">
+      Mon composant
+    </Title>
+    <input type="text"/>
+    <p>
+      Lorem
+    </p>
+    <ul>
+      {todos.map(todo => (<li key={todo}>{todo}</li>))}
+    </ul>
+  </>
+}
+
+function Title ({color, hidden, children, ...props}) {
+  if (hidden) {
+    return null
+  }
+
+  return <h1 style={{color: color}} {...props}>{children}</h1>
 }
 
 export default App
